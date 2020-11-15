8b8e25e616ee6ff41cba24619df0da34767f1e04
informatsy <informatsy@gmail.com>
2020-11-15 21:58:25 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 3000
+const app = express()
+  app.use(express.static(path.join(__dirname, './public')))
+  app.use(express.json({limit:'1mb'}))
+  
+  let data;
+  
+ app.post('/',(req,res)=>{
+    data = req.body
+   console.log(data)
+   res.json({
+    "success":"200 response",
+    "res":"You are now just talked with server" 
+   })
+  
+ })
+ 
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
